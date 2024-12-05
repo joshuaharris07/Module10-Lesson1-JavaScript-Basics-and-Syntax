9557bfd1d60249a0f803760557747aa8b01f2233
joshuaharris07 <joshua.harris07@gmail.com>
2024-12-05 16:53:57 -0700

    Added html and javascript for assignment.

---
+++ script.js
@@ -0,0 +1,8 @@
+let studentName = "Alice";
+let studentAge = 15;
+let studentGrade = 10;
+let isPresent = true;
+
+console.log(`The student's name is ${studentName}.`);
+console.log(`${studentName} is ${studentAge} years old and is in grade ${studentGrade}.`);
+console.log(`Is ${studentName} present? ${isPresent ? "Yes" : "No"}`);
